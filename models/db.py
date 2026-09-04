6ca1b7a1508877b5d91e6dd30c6e324856b6e0dd
abhyudayasingh127-ctrl <abhyudayasingh127@gmail.com>
2026-09-04 16:41:44 +0530

    Refactor DB connection to use environment variables

--- models/db.py
+++ models/db.py
@@ -1,10 +1,11 @@
+import os
 import mysql.connector
-from config import Config
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=Config.MYSQL_HOST,
-        user=Config.MYSQL_USER,
-        password=Config.MYSQL_PASSWORD,
-        database=Config.MYSQL_DB
+        host=os.getenv('MYSQL_HOST', 'localhost'),
+        user=os.getenv('MYSQL_USER', 'root'),
+        password=os.getenv('MYSQL_PASSWORD', ''),
+        database=os.getenv('MYSQL_DB', 'varanasi_tourism'),
+        port=int(os.getenv('MYSQL_PORT', 20357))
     )
