b2d5594cc54a0744827bf8d78e08b503423e164a
abhyudayasingh127-ctrl <abhyudayasingh127@gmail.com>
2026-09-04 16:34:39 +0530

    Add MYSQL_PORT configuration to config.py

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config:
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
     MYSQL_DB = os.getenv('MYSQL_DB', 'varanasi_tourism')
     OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY', '')
+    port=int(os.getenv('MYSQL_PORT', 3306))
